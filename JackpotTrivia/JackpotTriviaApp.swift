924661e729fc73e9c700fccf729c10a060a09ab0
coleywithdapoley <cobb.cole@gmail.com>
2026-05-20 19:16:28 -0400

    Initial Commit

---
+++ JackpotTrivia/JackpotTriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  JackpotTriviaApp.swift
+//  JackpotTrivia
+//
+//  Created by Cole Cobb on 5/20/26.
+//
+
+import SwiftUI
+
+@main
+struct JackpotTriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JackpotTriviaTests/JackpotTriviaTests.swift
@@ -0,0 +1,17 @@
+//
+//  JackpotTriviaTests.swift
+//  JackpotTriviaTests
+//
+//  Created by Cole Cobb on 5/20/26.
+//
+
+import Testing
+@testable import JackpotTrivia
+
+struct JackpotTriviaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
